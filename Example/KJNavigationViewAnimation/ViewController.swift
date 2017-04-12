cf5b100a21af2271e146d5732acc39757bbf5ea3
Kiran Jasvanee <mac241@Kiran-Jasvanee-MAC241.local>
2017-04-12 18:49:34 +0530

    done with example

--- Example/KJNavigationViewAnimation/ViewController.swift
+++ Example/KJNavigationViewAnimation/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import KJNavigationViewAnimation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewKJNavigation: KJNavigationViewAnimation!
+    @IBOutlet weak var tableview: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        viewKJNavigation.setupFor(Tableview: tableview,
+                                         viewController: self)
+        viewKJNavigation.topbarMinimumSpace = .none
+        viewKJNavigation.topbarMinimumSpaceCustomValue = 40
+        viewKJNavigation.isBlurrBackground = true
+        
+        tableview.dataSource = self
+        tableview.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,3 +35,33 @@ class ViewController: UIViewController {
 
 }
 
+
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate, UIScrollViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cellIdentifier = "cellIdentity1"
+        var cell = tableview.dequeueReusableCell(withIdentifier: cellIdentifier)
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
+        }
+        cell!.textLabel?.text = "Cell - \(indexPath.row)"
+        return cell!
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        viewKJNavigation.scrollviewMethod?.scrollViewDidScroll(scrollView)
+    }
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        viewKJNavigation.scrollviewMethod?.scrollViewWillBeginDragging(scrollView)
+    }
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        viewKJNavigation.scrollviewMethod?.scrollViewDidEndDragging(scrollView, willDecelerate: decelerate)
+    }
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        viewKJNavigation.scrollviewMethod?.scrollViewDidEndDecelerating(scrollView)
+    }
+}
